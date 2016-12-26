edacdb2b4b56a16e000a99ba04b36f81a6e8bd93
EmeraldMaster456 <sethcanonigo@yahoo.com>
2016-12-26 14:11:11 -0800

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,15 @@
+//Been to variables
+beentoforestopenning=true;
 
+//Items
+pickedupsword=false;
+
+//Current Room
+currentRoom="forest_openning";
+
+document.ready(function(){
+  $("form").submit(function(){
+    var input = $("#command_line").val();
+    
+  });
+});
