6cafa50f9ee71a1f759eaa562a40f20718528def
EmeraldMaster456 <sethcanonigo@yahoo.com>
2016-12-26 14:16:44 -0800

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -11,5 +11,8 @@ document.ready(function(){
   $("form").submit(function(){
     var input = $("#command_line").val();
     
+    if (input == "help"){
+      $("#message_help").clone().insertBefore("#placeholder").fadeIn(1500);
+        }
   });
 });
